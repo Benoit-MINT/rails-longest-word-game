8b431497881fe64c2b008fcbdc181cb08da37255
Benoit Queyron <queyronbenoit@gmail.com>
2024-01-28 16:03:18 +0100

    ex done

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,11 @@
+class GamesController < ApplicationController
+  def new
+    alphabet = ("a".."z").to_a
+    @letters = alphabet.sample(10)
+  end
+
+  def score
+    raise
+    params[:word]
+  end
+end
